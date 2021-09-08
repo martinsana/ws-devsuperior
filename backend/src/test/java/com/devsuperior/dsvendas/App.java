8340e08d9881b21060b1f402759c2a6030566a22
Ana Paula <anapaula.martins17@hotmail.com>
2021-09-08 16:53:17 -0300

    Project created

--- backend/src/test/java/com/devsuperior/dsvendas/App.java
+++ backend/src/test/java/com/devsuperior/dsvendas/App.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProjetoDesenvolvidoNaSemanaSpringReactDaDevsuperiorApplicationTests {
+class App {
 
 	@Test
 	void contextLoads() {
